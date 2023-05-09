b3fdcceef52df8930349bc74317e54c035fccb6f
lhuang09287750 <liang.huang@kyligence.io>
2023-05-09 18:13:14 +0800

    process exception and close db connection

--- retag.py
+++ retag.py
@@ -6,22 +6,23 @@ from core.connection.mysql_client import param
 if __name__ == '__main__':
 
     conn = pymysql.connect(**param)  # 连接对象
-    cur = conn.cursor()  # 游标对象，采用默认的数据格式
+    try:
+        with conn.cursor() as cur:  # 游标对象，采用默认的数据格式
+            sql0 = "select keywords,tag from error_category"
+            cur.execute(sql0)
+            results = cur.fetchall()
+            for row in results:
+                keywords = row[0].split(",")
+                tag_in_error_category = row[1]
+                kw_sql = ""
+                for kw in keywords:
+                    kw_sql = kw_sql + "locate('" + kw + "', gluten_result) > 0 and "
 
-    sql0 = "select keywords,tag from error_category"
-    cur.execute(sql0)
-    results = cur.fetchall()
-    for row in results:
-        keywords = row[0].split(",")
-        tag_in_error_category = row[1]
-        kw_sql = ""
-        for kw in keywords:
-            kw_sql = kw_sql + "locate('" + kw + "', gluten_result) > 0 and "
+                sql1 = "update inconsistent_record set tag='" + tag_in_error_category + "' where " \
+                    + kw_sql + "tag='unrecognized'"
+                cur.execute(sql1)
 
-        sql1 = "update inconsistent_record set tag='" + tag_in_error_category + "' where " \
-               + kw_sql + "tag='unrecognized'"
-        cur.execute(sql1)
-
-    conn.commit()
-    cur.close()
-    conn.close()
+            conn.commit()
+            cur.close()
+    finally:
+        conn.close()
