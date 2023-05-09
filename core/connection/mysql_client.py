09604f709b0a4a93b8e47500396863e5a393654b
lhuang09287750 <liang.huang@kyligence.io>
2023-05-09 17:35:22 +0800

    process exception and close db connection

--- core/connection/mysql_client.py
+++ core/connection/mysql_client.py
@@ -36,29 +36,32 @@ param = {
 
 def insert_into_inconsistent_record(*args):
     conn = pymysql.connect(**param)  # 连接对象
-    cur = conn.cursor()  # 游标对象，采用默认的数据格式
+    try:
+        with conn.cursor() as cur:  # 游标对象，采用默认的数据格式
 
-    tag = "unrecognized"
-    sql0 = "select keywords,tag from error_category"
-    cur.execute(sql0)
-    results = cur.fetchall()
-    gluten_result = args[2]
-    for row in results:
-        keywords = row[0]
-        tag_in_error_category = row[1]
-        error_matched = True
-        for kw in keywords.split(","):
-            error_matched = error_matched and operator.contains(gluten_result, kw)
-        if error_matched:
-            tag = tag_in_error_category
-            break
+            tag = "unrecognized"
+            sql0 = "select keywords,tag from error_category"
+            cur.execute(sql0)
+            results = cur.fetchall()
+            gluten_result = args[2]
+            for row in results:
+                keywords = row[0]
+                tag_in_error_category = row[1]
+                error_matched = True
+                for kw in keywords.split(","):
+                    error_matched = error_matched and operator.contains(gluten_result, kw)
+                if error_matched:
+                    tag = tag_in_error_category
+                    break
 
-    sql1 = "insert into inconsistent_record(project,query,gluten_result,normal_result,tag) values(%s, %s, %s, %s, %s)"
-    params = args + (tag,)
-    cur.execute(sql1, params)  # sql语句参数化，防止攻击！
-    conn.commit()
-    cur.close()  # 关闭游标
-    conn.close()  # 关闭连接
+            sql1 = "insert into inconsistent_record(project,query,gluten_result,normal_result,tag) " \
+                   "values(%s, %s, %s, %s, %s)"
+            params = args + (tag,)
+            cur.execute(sql1, params)  # sql语句参数化，防止攻击！
+            conn.commit()
+            cur.close()  # 关闭游标
+    finally:
+        conn.close()  # 关闭连接
 
 
 # CREATE TABLE response_time (
@@ -72,12 +75,12 @@ def insert_into_inconsistent_record(*args):
 # );
 def insert_into_response_time(*args):
     conn = pymysql.connect(**param)  # 连接对象
-    cur = conn.cursor()  # 游标对象，采用默认的数据格式
-    sql = "insert into response_time(project,query,gluten_res_time,normal_res_time) values(%s, %s, %s, %s)"
-    params = args
-    cur.execute(sql, params)  # sql语句参数化，防止攻击！
-    conn.commit()
-    cur.close()  # 关闭游标
-    conn.close()  # 关闭连接
-
-
+    try:
+        with conn.cursor() as cur:  # 游标对象，采用默认的数据格式
+            sql = "insert into response_time(project,query,gluten_res_time,normal_res_time) values(%s, %s, %s, %s)"
+            params = args
+            cur.execute(sql, params)  # sql语句参数化，防止攻击！
+            conn.commit()
+            cur.close()  # 关闭游标
+    finally:
+        conn.close()  # 关闭连接
