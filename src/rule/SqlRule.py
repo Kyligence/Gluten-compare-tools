b91c06e492806f5c772d0cbdca34f12242913cb4
Shuai.li <loneylee@live.cn>
2023-07-22 17:45:20 +0800

    ADD more sql rule

--- src/rule/SqlRule.py
+++ src/rule/SqlRule.py
@@ -1,4 +1,5 @@
 def is_stable_statement(statement: str) -> bool:
-    statement = statement.lower().replace("\r\n", " ").replace("\n", " ").replace("\r", " ")
+    statement = statement.lower().replace("\r\n", "\\r\\n").replace("\n", "\\n").replace("\r", "\\r")
 
-    return statement.find(" limit ") == -1
+    return statement.find(" limit ") == -1 and statement.find("\\nlimit\\n") == -1 \
+        and statement.find(" limit\\n") == -1 and statement.find("\\nlimit") == -1
