14ebfd7e5adb2ac9182affb5f10720134705d434
Shuai.li <loneylee@live.cn>
2023-09-13 15:42:48 +0800

    remove params controller save long running

--- collect_goreplay.py
+++ collect_goreplay.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 import os
 from datetime import date
@@ -10,19 +9,7 @@ from core.common import config
 from src.database.writer import CsvWriter, RedisWriter
 from src.entry.response import GoreplayReceive
 
-parser = argparse.ArgumentParser(description='command line arguments')
-parser.add_argument('--daily', type=bool,
-                    help='Save to daily dirs.', required=False,
-                    default=True)
-parser.add_argument('--long-running', type=bool,
-                    help='Save to long running redis key', required=False,
-                    default=True)
-
 app = Flask(__name__)
-
-args = vars(parser.parse_args())
-daily = args["daily"]
-long_running = args["long_running"]
 
 
 @app.route(config.FORWARD_PATH, methods=['POST'])
@@ -32,11 +19,8 @@ def endpoint():
 
 
 def dispatch(message: str):
-    if long_running:
-        save_long_running(message)
-
-    if long_running:
-        save_to_daily_file(message)
+    # save_long_running(message)
+    save_to_daily_file(message)
 
 
 def save_long_running(message: str):
